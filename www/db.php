3fdc290d7f730999e7b5924284d368372970268a
Aryo-41 <aryodewan162@gmail.com>
2020-03-29 14:32:33 +0700

    nama commit

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id12789094_mamun","sergioaguero16","id12789094_majumundur") or die ("could not connect database");
+?>
